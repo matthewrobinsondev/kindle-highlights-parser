387a17aec23d7b81dad97a9caec7615bdde09e27
Matt Robinson <matthewrobinsondev@gmail.com>
2024-10-14 22:32:52 +0100

    First attempt with Regex at parsing

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strings"
 )
 
 type Highlight struct {
@@ -19,11 +21,11 @@ func main() {
 	ParseClippings("My Clippings.txt")
 }
 
-func ParseClippings(filename string) {
+func ParseClippings(filename string) (map[string][]Highlight, error) {
 	file, err := os.Open(filename)
 
 	if err != nil {
-		return
+		return nil, err
 	}
 
 	defer file.Close()
@@ -36,5 +38,40 @@ func ParseClippings(filename string) {
 		lines = append(lines, scanner.Text())
 	}
 
-	fmt.Println(lines)
+	highlights := make(map[string][]Highlight)
+	var currentHighlight Highlight
+
+	title := regexp.MustCompile(`^(.*) \((.*)\)$`)
+	metaData := regexp.MustCompile(`Your Highlight.*page ([0-9]+) .*location ([0-9-]+) \| Added on (.*)`)
+
+	for i := 0; i < len(lines); i++ {
+		line := lines[i]
+		if title.MatchString(line) {
+			match := title.FindStringSubmatch(line)
+
+			currentHighlight.Title = match[1]
+			currentHighlight.Author = match[2]
+
+			continue
+		}
+
+		if metaData.MatchString(line) {
+			match := metaData.FindStringSubmatch(line)
+
+			currentHighlight.Page = match[1]
+			currentHighlight.Location = match[2]
+			currentHighlight.Date = match[3]
+
+			continue
+		}
+
+		if len(line) > 0 && !strings.HasPrefix(line, "==========") {
+			currentHighlight.Text = line
+			highlights[currentHighlight.Title] = append(highlights[currentHighlight.Title], currentHighlight)
+		}
+	}
+
+	fmt.Printf("%+v\n", highlights)
+
+	return highlights, nil
 }
--- main_test.go
+++ main_test.go
@@ -3,14 +3,25 @@ package main
 import (
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestParseClippings(t *testing.T) {
+	assert := assert.New(t)
 	filePath := "testData/Test Clippings.txt"
 
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
 		t.Fatalf("Test file not found: %v", err)
 	}
 
-	ParseClippings(filePath)
+	highlights, err := ParseClippings(filePath)
+
+	if err != nil {
+		t.Errorf("Unexpected error parising clippings")
+	}
+
+	assert.Equal(2, len(highlights), "There should be two highlights")
+	assert.Equal("Put more simply, a complex system like a digitized civilization is subject to cascading failures, where one thing depends on another, which depends on another thing.", highlights["Sandworm"][0].Text)
+	assert.Equal("Greenberg, Andy", highlights["Sandworm"][0].Author)
 }
